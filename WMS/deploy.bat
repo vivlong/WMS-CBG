cd438c69f51824c698729cb1b279c3ec43da744d
vivlong <vivlonglz@gmail.com>
2016-04-01 14:05:45 +0800

    Update deploy.bat

--- WMS/deploy.bat
+++ WMS/deploy.bat
@@ -1,7 +1,7 @@
 @echo on
 
-xcopy /y/e/s www \\192.168.0.230\wwwroot\mobileapp-wh-cbg\www
-copy /y index.html \\192.168.0.230\wwwroot\mobileapp-wh-cbg
+xcopy /y/e/s www \\192.168.0.230\wwwroot\mobileapp-wh-cbg\demo\www
+copy /y index.html \\192.168.0.230\wwwroot\mobileapp-wh-cbg\demo
 copy /y update.json \\192.168.0.230\wwwroot\mobileapp-wh-cbg
 copy /y WMS.apk \\192.168.0.230\wwwroot\mobileapp-wh-cbg\WMS-CBG.apk
 del WMS.apk /f /q
