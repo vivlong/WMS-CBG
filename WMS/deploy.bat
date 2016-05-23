11c4af83112fda32b2d73c4c1f1a4b3358d6f2f0
vivlong <vivlonglz@gmail.com>
2016-05-23 13:27:23 +0800

    Update deploy.bat

--- WMS/deploy.bat
+++ WMS/deploy.bat
@@ -1,9 +1,9 @@
 @echo on
-
-xcopy /y/e/s www \\192.168.0.230\wwwroot\mobileapp-wh\demo\www
-copy /y index.html \\192.168.0.230\wwwroot\mobileapp-wh\demo
-copy /y update.json \\192.168.0.230\wwwroot\mobileapp-wh
-copy /y WMS.apk \\192.168.0.230\wwwroot\mobileapp-wh\WMS-CBG.apk
+set target="\\192.168.0.230\wwwroot\app\wms\interglobo"
+xcopy /y/e/s www %target%\www
+copy /y index.html %target%
+copy /y update.json %target%
+copy /y WMS.apk %target%\WMS-SingleSN.apk
 del WMS.apk /f /q
 
 pause 
